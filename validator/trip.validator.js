38e3ba69d789fb3a833ddf54addb732e72694634
ncherryu <alswls8345@naver.com>
2024-07-05 12:58:05 +0900

    fix: 유효성 메시지 오타 수정

--- validator/trip.validator.js
+++ validator/trip.validator.js
@@ -4,8 +4,8 @@ const tripIdValidate = param('tripId').isLength({ min: 24, max: 24 }).withMessag
 const titleValidate = body('title').notEmpty().withMessage('제목을 입력해주세요.');
 const dateValidate = body('date').notEmpty().isDate().withMessage('날짜를 입력해주세요.');
 const locationValidate = body('location').notEmpty().withMessage('장소를 입력해주세요.');
-const xCoordinateValidate = body('xCoordinate').notEmpty().isNumeric().withMessage('제목을 입력해주세요.');
-const yCoordinateValidate = body('xCoordinate').notEmpty().isNumeric().withMessage('제목을 입력해주세요.');
+const xCoordinateValidate = body('xCoordinate').notEmpty().isNumeric().withMessage('x좌표를 입력해주세요.');
+const yCoordinateValidate = body('xCoordinate').notEmpty().isNumeric().withMessage('y좌표를 입력해주세요.');
 
 const tripDataValidate = [
     titleValidate,
