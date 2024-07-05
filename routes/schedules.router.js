3e7263f2ad64e940a522d52f6bc2858090b5a93e
ncherryu <alswls8345@naver.com>
2024-07-05 14:41:23 +0900

    feat: 일정 API 유효성 검사 추가

--- routes/schedules.router.js
+++ routes/schedules.router.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const { validateToken } = require('../authorization');
 const { createSchedule, changeSchedule, removeSchedule } = require('../controllers/schedule.controller');
+const { tripIdValidate } = require('../validator/trip.validator');
+const { createScheduleValidator, scheduleIdValidate, updateScheduleValidator } = require('../validator/schedule.validator');
+const { validate } = require('../validator/validate');
 const router = express.Router();
 
 router.post(
     '/:tripId/schedules',
-    [validateToken],
+    [validateToken, tripIdValidate, createScheduleValidator, validate],
     createSchedule
 );
 
 router.put(
     '/:tripId/schedules/:scheduleId',
-    [validateToken],
+    [validateToken, tripIdValidate, scheduleIdValidate, updateScheduleValidator, validate],
     changeSchedule
 );
 
 router.delete(
     '/:tripId/schedules/:scheduleId',
-    [validateToken],
+    [validateToken, tripIdValidate, scheduleIdValidate],
     removeSchedule
 );
 
---
+++ validator/schedule.validator.js
@@ -0,0 +1,32 @@
+const { body, param } = require('express-validator');
+
+const scheduleIdValidate = param('scheduleId').isLength({ min: 24, max: 24 }).withMessage('존재하지 않는 일정입니다.');
+const locationValidate = body('location').notEmpty().withMessage('장소를 입력해주세요.');
+const xCoordinateValidate = body('xCoordinate').notEmpty().isNumeric().withMessage('x좌표를 입력해주세요.');
+const yCoordinateValidate = body('yCoordinate').notEmpty().isNumeric().withMessage('y좌표를 입력해주세요.');
+const isCheckedValidate = body('isChecked').notEmpty().isBoolean().withMessage('체크 여부를 입력해주세요.');
+
+const isValidDateTime = (value) => {
+    const date = new Date(value);
+    if (date == 'Invalid Date') {
+        throw new Error('시간을 제대로 입력해주세요.');
+    }
+    return true;
+}
+
+
+const startTimeValidate = body('startTime').custom(isValidDateTime);
+const endTimeValidate = body('endTime').custom(isValidDateTime);
+
+
+const createScheduleValidator = [
+    locationValidate,
+    xCoordinateValidate,
+    yCoordinateValidate,
+    startTimeValidate,
+    endTimeValidate
+];
+
+const updateScheduleValidator = [...createScheduleValidator, isCheckedValidate];
+
+module.exports = { scheduleIdValidate, createScheduleValidator, updateScheduleValidator };
