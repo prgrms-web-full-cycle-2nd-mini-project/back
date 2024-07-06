af23d5fdb5087048409674fd1160cf287f1359b8
tbs01215 <tbs01215@gmail.com>
2024-07-06 17:16:50 +0900

    refactor: 인증을 req.headers에서 req.cookies로 변경

--- app.js
+++ app.js
@@ -1,45 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const { StatusCodes } = require("http-status-codes");
-const dotenv = require('dotenv');
-const connectDB = require('./db');
-const cors = require('cors');
+const dotenv = require("dotenv");
+const connectDB = require("./db");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(cookieParser());
+
 dotenv.config();
 connectDB();
 
 const corsOptions = {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
-    optionsSuccessStatus: 204,
-    credentials: true,
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
+  optionsSuccessStatus: 204,
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
-const userRouter = require('./routes/users.router');
-const tripRouter = require('./routes/trips.router');
-const scheduleRouter = require('./routes/schedules.router');
+const userRouter = require("./routes/users.router");
+const tripRouter = require("./routes/trips.router");
+const scheduleRouter = require("./routes/schedules.router");
 
-app.use('/users', userRouter);
-app.use('/trips', tripRouter);
-app.use('/trips', scheduleRouter);
+app.use("/users", userRouter);
+app.use("/trips", tripRouter);
+app.use("/trips", scheduleRouter);
 
-app.listen(process.env.PORT, () => console.log(`Server listen on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server listen on port ${process.env.PORT}`)
+);
 
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = StatusCodes.NOT_FOUND;
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = StatusCodes.NOT_FOUND;
 
-    next(error);
-})
+  next(error);
+});
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const errMsg = err.message || '서버 내부 오류 발생';
-    console.error(`에러 발생: ${err.name}`);
-    console.error(`에러 메시지: ${err.message}`);
-    console.error(`에러 스택: ${err.stack}`);
-    return res.status(statusCode).json({ message: errMsg });
-})
+  const statusCode = err.statusCode || 500;
+  const errMsg = err.message || "서버 내부 오류 발생";
+  console.error(`에러 발생: ${err.name}`);
+  console.error(`에러 메시지: ${err.message}`);
+  console.error(`에러 스택: ${err.stack}`);
+  return res.status(statusCode).json({ message: errMsg });
+});
--- authorization.js
+++ authorization.js
@@ -1,81 +1,82 @@
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const { StatusCodes } = require('http-status-codes');
-const Trip = require('./models/trips.model');
-const { CustomError } = require('./utils/CustomError');
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const { StatusCodes } = require("http-status-codes");
+const Trip = require("./models/trips.model");
+const { CustomError } = require("./utils/CustomError");
 
 dotenv.config();
 
 function ensureAuthorization(req) {
-    try {
-        const receivedJwt = req.headers['authorization'];
+  try {
+    const token = req.cookies.token; // 쿠키에서 JWT 추출
 
-        if (receivedJwt) {
-            const decodedJwt = jwt.verify(receivedJwt, process.env.JWT_PRIVATE_KEY);
-            return decodedJwt;
-        } else {
-            throw new ReferenceError('jwt must be provided');
-        }
-
-    } catch (err) {
-        return err;
+    if (token) {
+      const decodedJwt = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+      return decodedJwt;
+    } else {
+      throw new ReferenceError("JWT must be provided");
     }
+  } catch (err) {
+    return err;
+  }
 }
 
 function validateToken(req, res, next) {
-    const authorization = ensureAuthorization(req);
-    if (authorization instanceof jwt.TokenExpiredError) {
-        return res.status(StatusCodes.UNAUTHORIZED).json({
-            message: '로그인 세션이 만료되었습니다. 다시 로그인 하세요.'
-        });
-    } else if (authorization instanceof jwt.JsonWebTokenError) {
-        return res.status(StatusCodes.UNAUTHORIZED).json({
-            message: '잘못된 토큰입니다.'
-        });
-    } else if (authorization instanceof ReferenceError) {
-        return res.status(StatusCodes.UNAUTHORIZED).json({
-            message: '로그인이 필요한 기능입니다.'
-        });
-    }
+  const authorization = ensureAuthorization(req);
 
-    return next();
+  if (authorization instanceof jwt.TokenExpiredError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "로그인 세션이 만료되었습니다. 다시 로그인 하세요.",
+    });
+  } else if (authorization instanceof jwt.JsonWebTokenError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "잘못된 토큰입니다.",
+    });
+  } else if (authorization instanceof ReferenceError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "로그인이 필요한 기능입니다.",
+    });
+  }
+
+  req.user = authorization; // 인증된 사용자 정보를 요청 객체에 저장
+  return next();
 }
 
 function getLoginedId(req) {
-    const authorization = ensureAuthorization(req);
-    if (authorization.id) {
-        return authorization.id;
-    }
+  const authorization = ensureAuthorization(req);
+  if (authorization && authorization.id) {
+    return authorization.id;
+  }
 
-    return false;
+  return false;
 }
 
 async function accessAuthorization(req) {
-    try {
-        const { tripId } = req.params;
-        const authorization = ensureAuthorization(req);
+  try {
+    const { tripId } = req.params;
+    const authorization = ensureAuthorization(req);
 
-        const trip = await Trip.findById(tripId);
-        if (!trip) {
-            throw new CustomError(
-                '존재하지 않는 여행입니다.',
-                StatusCodes.NOT_FOUND,
-            );
-        }
-
-        if (authorization.id !== trip.owner.toString()) {
-            throw new CustomError(
-                '접근 권한이 없습니다.',
-                StatusCodes.UNAUTHORIZED,
-            );
-        }
-    } catch (err) {
-        throw new CustomError(
-            err.message || '인증 실패',
-            err.statusCode || StatusCodes.UNAUTHORIZED,
-            err
-        )
+    const trip = await Trip.findById(tripId);
+    if (!trip) {
+      throw new CustomError("존재하지 않는 여행입니다.", StatusCodes.NOT_FOUND);
     }
+
+    if (authorization.id !== trip.owner.toString()) {
+      throw new CustomError("접근 권한이 없습니다.", StatusCodes.UNAUTHORIZED);
+    }
+  } catch (err) {
+    throw new CustomError(
+      err.message || "인증 실패",
+      err.statusCode || StatusCodes.UNAUTHORIZED,
+      err
+    );
+  }
 }
 
-module.exports = { ensureAuthorization, validateToken, getLoginedId, accessAuthorization };
+module.exports = {
+  ensureAuthorization,
+  validateToken,
+  getLoginedId,
+  accessAuthorization,
+};
