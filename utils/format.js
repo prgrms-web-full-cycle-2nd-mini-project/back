6abd7a6edfdef94b0a3fe60f0f584a4ce793c468
ncherryu <alswls8345@naver.com>
2024-07-13 15:13:50 +0900

    refector: 일정의 년월일 값 고정

--- utils/format.js
+++ utils/format.js
@@ -1,6 +1,7 @@
 const convertUTC = (time) => {
     const newTime = new Date(time + '.000Z');
     newTime.setHours(newTime.getHours() - 9);
+    newTime.setFullYear(2024, 6, 20);
 
     return newTime;
 }
